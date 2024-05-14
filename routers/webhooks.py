07ee06d377f1deb7e7bffc5f41b2ab3ba5e29a81
David Nguyen <davidn@cisco.com>
2024-05-14 09:51:04 -0700

    Change route name to process webhook

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -40,7 +40,7 @@ async def remove_webhook(name: str, db: any = Depends(get_database)):
     return await delete_webhook(db, name)
 
 
-@webhooks.post("/name={name}")
+@webhooks.post("/process/{name}")
 async def process_webhook(name: str, body: WebhookPayload, db: any = Depends(get_database)):
   print(f"Webhook name = {name}")
   webhook = await get_webhook(db, name)
