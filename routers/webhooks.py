1052a8e648933fa8d5e5ac2d020d96d80c9671a2
David Nguyen <davidn@cisco.com>
2024-05-22 09:11:21 -0700

    Process payload type for Common Room webhooks

--- routers/webhooks.py
+++ routers/webhooks.py
@@ -41,15 +41,24 @@ async def remove_webhook(name: str, db: any = Depends(get_database)):
 
 @webhooks.post("/process/{name}")
 async def process_webhook(name: str, body: WebhookPayload, db: any = Depends(get_database)):
-  print(f"Webhook name = {name}")
-  webhook: Webhook = await get_webhook(db, name)
-  msg_template = await get_template(db,webhook.template)
-  if msg_template:
-      send_message_to_room(webhook.roomId, dict(body.payload), msg_template)
-      return {"message": "webhook processed successfully!"}
-  else:
-      return {"message": "can't process webhook"}
-  
+    print(f"Webhook name = {name}")
+    payload = dict(body.payload)
+    print(f"payload type={payload['type']}")
+    if payload['type'] == "activity":
+        webhook: Webhook = await get_webhook(db, name)
+        msg_template = await get_template(db,webhook.template)
+        if msg_template:
+            send_message_to_room(webhook.roomId, dict(body.payload), msg_template)
+            return {"message": "webhook processed successfully!"}
+        else:
+            return {"message": "can't process webhook"}
+    elif payload['type'] == "member":
+        return {"message": f"received member payload for {payload['fullName']}"}
+    elif payload['type'] == "organization":
+        return {"message": f"received organization payload for {payload['name']}"}
+    else:
+        return {"message": f"unknown payload type {payload['type']}"}
+    
 @webhooks.post("/registration")
 async def process_devnet_new_registration(new_user: WebhookDevNetNewRegistration):
     print("Receive new DevNet user registration")
